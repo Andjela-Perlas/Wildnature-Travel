bcf668348633f080f077021cc410aa366156d819
Andjela Perlas <38106481+Andjela-Perlas@users.noreply.github.com>
2021-12-12 20:03:48 +0100

    Update json.js

--- resources/js/json.js
+++ resources/js/json.js
@@ -7,9 +7,9 @@ var apartments = [
     },
     "info": {
       "name" : "Vikendica Zlatni prag",
-      "persons" : "2-3 osobe",
-      "rooms" : "Sobe: dvokrevetne/ trokrevetne",
-      "price": "25 EUR/noćenje"
+      "persons" : "2 - 3 osobe",
+      "rooms" : "Sobe: dvokrevetne / trokrevetne",
+      "price": "25 EUR / noćenje"
     },
     "description": "Vikendica nesebično nudi kompletno opremljen smeštaj sa odvojenim ulazom, za vaš ugodan i prijatan boravak u oazi netaknute prirode naše najlepše planine. Ispred apartmana nalaze se prostrane terase za vaše potpuno uživanje, obed ili kafu uz prelep pogled ka jezeru."
   },
@@ -21,9 +21,9 @@ var apartments = [
     },
     "info": {
       "name" : "Vikendica Tarsko srce",
-      "persons" : "3-4 osobe",
-      "rooms" : "Sobe: trokrevetne/ četvorokrevetne",
-      "price": "35 EUR/noćenje"
+      "persons" : "3 - 4 osobe",
+      "rooms" : "Sobe: trokrevetne / četvorokrevetne",
+      "price": "35 EUR / noćenje"
     },
     "description": " Vikendica TARSKO SRCE nalazi se na Osluši, u centralnom delu planine Tare, na 1030m nadmorske visine. Udaljena je 4km od Mitrovca, gde se nalazi i najbliža prodavnica i ambulanta. Svojim komforom i udobnošću pružiće Vam sve za savršen odmor na najzdravijem delu planine Tare."
   },
@@ -36,8 +36,8 @@ var apartments = [
     "info": {
       "name" : "Hotel sa 4 zvezdice",
       "persons" : "više osoba",
-      "rooms" : "Sobe: dvokrevetne/četvorokrevetne",
-      "price": "35 EUR/noćenje"
+      "rooms" : "Sobe: dvokrevetne / četvorokrevetne",
+      "price": "35 EUR / noćenje"
     },
     "description": "U samom centru parka Tara, svega nekoliko minuta hoda od glavnog šetališta, restorana i kafića. U sklopu hotela postoji i recepcija, odmah na ulazu u objekat, gde goste dočekuje naše ljubazno osoblje i po potrebi pruža sve neophodne informacije i pomoć našim dragim gostima."
   }
